dced21eada27849555f4a11fdeefb8da94b07362
Robert Trifan <trifangrobert@gmail.com>
2022-09-09 02:27:36 +0300

    css for check and mouse over

--- src/chess/PvP.js
+++ src/chess/PvP.js
@@ -10,8 +10,21 @@ const PvP = () => {
   );
   // const [squareStyles, setSquareStyles] = useState({'e2': {backgroundColor: 'orange'}});
   const [squareStyles, setSquareStyles] = useState();
+  const getKingPositionInCheck = (game) => {
+    let kingPosition;
+    for (let i = 0;i < 8;++i) {
+      for (let j = 0;j < 8;++j) {
+        let piece = game.board()[i][j];
+        if (piece !== null && piece.type === 'k' && piece.color === game.turn()) {
+          kingPosition = piece.square;
+        }
+      }
+    }
+    // console.log(kingPosition);
+    return kingPosition;
+  }
   const handleOnDrop = ({ sourceSquare, targetSquare }) => {
-    console.log("handle on drop");
+    // console.log("handle on drop");
     let move = game.move({
       from: sourceSquare,
       to: targetSquare,
@@ -24,16 +37,22 @@ const PvP = () => {
   };
   const highlightSquare = (square) => {
     let moves = game.moves({ square: square, verbose: true });
-    console.log(square, moves, typeof(square));
+    // console.log(square, moves, typeof(square));
     let newStyles = {};
     newStyles[square] = { background: "rgba(20,85,30,0.5)" };
-    console.log(newStyles);
+    // console.log(newStyles);
     for (let i = 0;i < moves.length;++i) {
       newStyles[moves[i].to] = {background: "radial-gradient(rgba(20,85,30,0.5) 19%, rgba(0,0,0,0) 20%)"};
     }
-    console.log(newStyles);
+    // console.log(newStyles);
+    // radial-gradient(ellipse at center, red 0%, #e70000 25%, rgba(169,0,0,0) 89%, rgba(158,0,0,0) 100%); css for check
     if (game.get(square) === null || game.get(square).color !== game.turn()) {
       newStyles = {};
+    }
+    let kingPosition = getKingPositionInCheck(game);
+    if (game.in_check()) {
+      newStyles[kingPosition] = {background: "radial-gradient(ellipse at center, red 0%, #e70000 25%, rgba(169,0,0,0) 89%, rgba(158,0,0,0) 100%)"};
+      // newStyles[kingPosition] = {background: "red"}
     }
     setSquareStyles((prevSquareStyles) => ({
       ...newStyles
@@ -42,16 +61,16 @@ const PvP = () => {
   const removeHighlightSquare = () => {
     setSquareStyles({});
   }
-  const handleOnSquareClick = (square) => {
+  const handleOnMouseOverSquare = (square) => {
     removeHighlightSquare();
     highlightSquare(square);
-  };
+  }
   return (
     <div>
       <Chessboard
         position={position}
         onDrop={handleOnDrop}
-        onSquareClick={handleOnSquareClick}
+        onMouseOverSquare={handleOnMouseOverSquare}
         squareStyles={squareStyles}
       />
     </div>
